c79d42d022d2bfd7814663614fc42c6e8b35550a
Emivw <emiya.vanwyk@gmail.com>
2022-04-22 23:34:11 +0200

    testomony added

--- css/java.js
+++ css/java.js
@@ -73,3 +73,37 @@ var TxtType = function(el, toRotate, period) {
       css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
       document.body.appendChild(css);
   };
+
+
+
+//   testomonials
+const sliderElm = document.querySelector(".slider-container .slider");
+const btnLeft = document.querySelector(".slider-container .btn-left");
+const btnRight = document.querySelector(".slider-container .btn-right");
+
+const numberSliderBoxs = sliderElm.children.length;
+let idxCurrentSlide = 0;
+
+// Functions:
+function moveSlider() {
+  let leftMargin = (sliderElm.clientWidth / numberSliderBoxs) * idxCurrentSlide;
+  sliderElm.style.marginLeft = -leftMargin + "px";
+  console.log(sliderElm.clientWidth, leftMargin);
+}
+function moveLeft() {
+  if (idxCurrentSlide === 0) idxCurrentSlide = numberSliderBoxs - 1;
+  else idxCurrentSlide--;
+
+  moveSlider();
+}
+function moveRight() {
+  if (idxCurrentSlide === numberSliderBoxs - 1) idxCurrentSlide = 0;
+  else idxCurrentSlide++;
+
+  moveSlider();
+}
+
+// Event Listeners:
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+window.addEventListener("resize", moveSlider);
